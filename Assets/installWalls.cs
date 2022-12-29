98ff8ed8a749c339bd8a78a6f479bcd904a4d486
Scott S <ssnyde16@asu.edu>
2022-12-29 15:31:49 -0700

    fixed conditional commands

--- Assets/installWalls.cs
+++ Assets/installWalls.cs
@@ -11,30 +11,131 @@ public class installWalls : MonoBehaviour
     [SerializeField] private GameObject formwork;
     [SerializeField] private GameObject hvacBlocks;
     [SerializeField] private GameObject plumbing;
+    [SerializeField] private GameObject fullWall;
+    /*
+     * if else statement to check if any of their own objects exist already
+     * 
+     */
 
+    public void win()
+    {
+        if(GameObject.Find("hvac(Clone)") && GameObject.Find("plumbing(Clone)"))
+        {
+            Debug.Log("You Win!! NICE!!!!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+    //utility function to check if everything is done prior to 'curve ball'
+    public bool checkEmbedsElectrical()
+    {
+        if(GameObject.Find("rebar(Clone)") && GameObject.Find("electrical(Clone)") && GameObject.Find("embeds(Clone)") )
+        {
+            return true;
+        }
+        return false;
+    }
     public void InstallRebar()
     {
+        //if else to prevent duplicate objects of same type
+        if( GameObject.Find("rebar(Clone)") )
+        {
+        } else
+        {
             Instantiate(rebar, rebar.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        }
     }
     public void InstallElectrical()
     {
+        if ( GameObject.Find("electrical(Clone)") )
+        {
+        }
+        else
+        {
+            if (checkEmbedsElectrical())
+            {
+                Debug.Log("THIS IS NOW WHERE AN EVENT NEEDS TO OCCUR TO LET STUDENT KNOW THAT THERE NEEDS TO BE A DILVERY");
+            }
             Instantiate(electrical, electrical.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        }
+        
     }
     public void InstallEmbeds()
     {
-            Instantiate(embeds, embeds.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        if ( GameObject.Find("embeds(Clone)") )
+        {
+        }
+        else
+        {
+            //nested if to make sure it can be called as to flow chart
+            if (GameObject.Find("rebar(Clone)"))
+            {
+                Instantiate(embeds, embeds.transform.position, Quaternion.Euler(0f, 180f, 0f));
+                if(checkEmbedsElectrical())
+                {
+                    Debug.Log("THIS IS NOW WHERE AN EVENT NEEDS TO OCCUR TO LET STUDENT KNOW THAT THERE NEEDS TO BE A DILVERY");
+                }
+            } else
+            {
+                //maybe a failure instance
+                Debug.Log("Cant install embeds until rebar is placed");
+            }
+            
+        }
+        
     }
     public void InstallFormwork()
     {
-            Instantiate(formwork, formwork.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        if (GameObject.Find("formwork(Clone)"))
+        {
+        }
+        else
+        {
+            if (checkEmbedsElectrical())
+            {
+                Instantiate(formwork, formwork.transform.position, Quaternion.Euler(0f, 180f, 0f));
+            }
+            
+        }
+        
     }
     public void InstallHVACBlocks()
     {
-            Instantiate(hvacBlocks, hvacBlocks.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        
+        if ( GameObject.Find("hvac(Clone)") )
+        {
+        }
+        else
+        {
+            if (GameObject.Find("formwork(Clone)") )
+            {
+                Instantiate(hvacBlocks, hvacBlocks.transform.position, Quaternion.Euler(0f, 180f, 0f));
+                win();
+            }
+            else
+            {
+                Debug.Log("You need to have formwork first in order to do this");
+            }
+            
+        }
+        
     }
     public void InstallPlumbing()
     {
-            Instantiate(plumbing, plumbing.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        if ( GameObject.Find("plumbing(Clone)") )
+        {
+        }
+        else
+        {
+            if (GameObject.Find("formwork(Clone)") )
+            {
+                Instantiate(plumbing, plumbing.transform.position, Quaternion.Euler(0f, 180f, 0f));
+                win();
+            } else
+            {
+                Debug.Log("You need to have formwork first in order to do this");
+            }
+            
+        }
+        
     }
 
     // Update is called once per frame
@@ -45,7 +146,7 @@ public class installWalls : MonoBehaviour
 
     void Start()
     {
-        var star = Instantiate(baseWall, baseWall.transform.position, Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(baseWall, baseWall.transform.position, Quaternion.Euler(0f, 180f, 0f));        
     }
 
 }
