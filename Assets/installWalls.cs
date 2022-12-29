f45810bb7cf46217a83acdab7470a6bddee60f68
Scott S <ssnyde16@asu.edu>
2022-12-29 09:41:31 -0700

    test
    test

--- Assets/installWalls.cs
+++ Assets/installWalls.cs
@@ -14,54 +14,27 @@ public class installWalls : MonoBehaviour
 
     public void InstallRebar()
     {
-        if (GameObject.Find("base-wall"))
-        {
             Instantiate(rebar, rebar.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
     }
     public void InstallElectrical()
     {
-        if (GameObject.Find("base-wall"))
-        {
             Instantiate(electrical, electrical.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
     }
     public void InstallEmbeds()
     {
-        if (GameObject.Find("wall-2"))
-        {
             Instantiate(embeds, embeds.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
-        else
-        {
-            Destroy(Instantiate(embeds, embeds.transform.position, Quaternion.Euler(0f, 180f, 0f)), 5);
-
-        }
-        
     }
     public void InstallFormwork()
     {
-        if (GameObject.Find("wall-3"))
-        {
             Instantiate(formwork, formwork.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
-        
     }
     public void InstallHVACBlocks()
     {
-        if (GameObject.Find("wall-4"))
-        {
             Instantiate(hvacBlocks, hvacBlocks.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
-        
     }
     public void InstallPlumbing()
     {
-        if (GameObject.Find("wall-5"))
-        {
             Instantiate(plumbing, plumbing.transform.position, Quaternion.Euler(0f, 180f, 0f));
-        }
-        
     }
 
     // Update is called once per frame
